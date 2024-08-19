fc43f965150f43aa86ca5d4fcf7802cc7690e680
t0khyo <abdullrhman.eltoukhy@gmail.com>
2024-08-19 21:37:04 +0300

    feat: Logging Aspect

---
+++ src/main/java/com/t0khyo/library/aspect/LoggingAspect.java
@@ -0,0 +1,38 @@
+package com.t0khyo.library.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("execution(public * com.t0khyo.library.service.impl.*.*(..))")
+    public void serviceMethodsLoggingPackage() {
+        // This method is intentionally left blank
+        // It's used only as a pointcut reference
+    }
+
+    @Around("serviceMethodsLoggingPackage()")
+    public Object logAroundService(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        log.info(">> {}.{}() - {}", className, methodName, Arrays.toString(args));
+
+        Object result = joinPoint.proceed();
+
+        log.info("<< {}.{}() - {}", className, methodName, result);
+
+        return result;
+    }
+}
+
