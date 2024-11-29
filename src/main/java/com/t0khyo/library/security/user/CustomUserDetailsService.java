bc174f4dfddab4010fa93fa0bcf6f963e49e4c2b
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2024-11-29 09:11:51 +0200

    feat: CustomUserDetailsService

--- src/main/java/com/t0khyo/library/security/user/CustomUserDetailsService.java
+++ src/main/java/com/t0khyo/library/security/user/CustomUserDetailsService.java
@@ -16,6 +16,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .map(CustomUserDetails::new)
-                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
     }
 }
