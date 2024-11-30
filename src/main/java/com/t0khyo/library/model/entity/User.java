2d48c42813a05f5f165cdbe4dcaa683d3cc2caeb
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2024-11-30 20:28:34 +0200

    feat: initial roles in User entity

--- src/main/java/com/t0khyo/library/model/entity/User.java
+++ src/main/java/com/t0khyo/library/model/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -41,5 +42,5 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     @ToString.Exclude
-    private Set<Role> roles;
+    private Set<Role> roles = new HashSet<>();
 }
