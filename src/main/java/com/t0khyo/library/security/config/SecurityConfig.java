e883bb2541979d50e02e6b562d5470c7e56758dd
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2024-12-01 21:23:15 +0200

    feat: session policy in SecurityConfig

--- src/main/java/com/t0khyo/library/security/config/SecurityConfig.java
+++ src/main/java/com/t0khyo/library/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
@@ -28,6 +29,8 @@ public class SecurityConfig {
                                     .permitAll();
                             authorize.anyRequest().authenticated();
                         })
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
